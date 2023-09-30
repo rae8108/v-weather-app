7523da51e021794cfbfcdd9d16c5ea7cb72d586a
rae8108 <146367231+rae8108@users.noreply.github.com>
2023-09-30 21:29:03 +0100

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#city");
@@ -11,6 +10,52 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
 }
+
+let now = new Date();
+let h2 = document.querySelector("h2");
+let hours = now.getHours();
+
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
+let minutes = now.getMinutes();
+
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let day = days[now.getDay()];
+let month = months[now.getMonth()];
+let date = now.getDate();
+let year = now.getFullYear();
+
+h2.innerHTML = `${day} ${month} ${date}, ${year} ${hours}:${minutes}`;
 
 let key = "8b0d9fa4b250096d3e2803d0f00fa";
 let apiUrl =
